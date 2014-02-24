40d39ad8c7ba2a569786313b36bc964b956b01e2
Viktor Konovalov <kde3.5virpool@gmail.com>
2014-02-24 18:43:09 +0200

    Colorize output of console based logger

--- app/logger/console.js
+++ app/logger/console.js
@@ -1,1 +1,23 @@
-module.exports = console;
+var colors = require('colors');
+
+var theme = {
+    trace: 'cyan',
+    info: 'green',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+};
+colors.setTheme(theme);
+
+function makeLevel(level) {
+    return function () {
+        console[level](Array.prototype.slice.call(arguments, 0).join(' ')[level]);
+    }
+}
+
+var logger = {};
+Object.keys(theme).forEach(function (lvl) {
+    logger[lvl] = makeLevel(lvl);
+});
+
+module.exports = logger;
